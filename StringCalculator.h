06d0850b7f127200e5f4f43483ae4fca2b81fac8
NikitaChittimalla <120707107+NikitaChittimalla@users.noreply.github.com>
2024-08-12 12:10:26 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,13 +1,65 @@
-boolean isEmptyString(const char*input){
-if (input == NULL || input[0] == '\0') {
-        return TRUE; // The string is empty
-    }
-return FALSE;
-}
-int add(const char*input)
+#include <stdlib.h>
+#include <string.h>
+
+int isNumGreaterThanThousand(int num)
 {
-  if (isEmptyString(input)){return 0;}
- // if given input contains single number which is < 1000 and unsigned with no delimiters then return same number
-    
-return -1;
+    if(num > 1000)
+    {
+        return 0;
+    }
+    return num;
+}
+
+int tokenizeString(char *input)
+{
+    char *token;
+    int total = 0;
+    const char delimiters[] = "$#\n:,;//\\&#%";
+
+    token = strtok(input, delimiters);
+    while (token != NULL) 
+    {
+        // Convert token to integer using atoi
+        int num = atoi(token);
+
+        total += isNumGreaterThanThousand(num);
+        // Get the next token
+        token = strtok(NULL, delimiters);
+    }
+
+    return total;
+}
+
+int createCopy(const char *input)
+{
+    if (strlen(input) == 1)
+    {
+        return atoi(input);
+    }
+    // Create a modifiable copy of the input
+    char inputCopy[100];
+    strcpy(inputCopy, input);
+    return tokenizeString(inputCopy); // Provide sum only if it is not a single character
+}
+
+int isEmptyString(const char* input)
+{
+  if (input == NULL || input[0] == '\0') 
+  {
+     return 1; 
+  }
+  return 0;
+}
+}
+
+int add (const char* str)
+{
+  if(isEmptyString(str))
+  {
+     return 0;
+  }
+  else
+  {
+    return createCopy(str);
+  }
 }
